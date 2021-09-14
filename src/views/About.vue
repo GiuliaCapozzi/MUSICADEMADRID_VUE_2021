4de843ca3be79100f7cedf87d21661fa7e3387b3
giuliaCapozzi <giulscapozzi2.0@gmail.com>
2021-09-14 20:43:41 +0200

    enlace ux

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1><a href="https://musicademadrid.es">musicademadrid.es</a></h1>
+    <p><a href="https://uxplanet.org/livestream-music-concert-app-a-ui-ux-case-study-1b4ac5227c83">Enlace a la ux de inspiración para la nueva maquetación</a></p>
     <p>...Es un proyecto que nació en 2019. Su UX/UI ya está para cambiar, y aquí tenemos los primeros pasos hacia su nuevo concepto. Enjoy :)</p>
   </div>
 </template>
